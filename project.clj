d03e12672a2d9bedacbf8335d408277873d296d4
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-28 10:49:42 +0200

    lein-release plugin: bumped version from 0.1.2 to 0.1.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.domaindrivenarchitecture/dda-pallet-commons "0.1.2"
+(defproject org.domaindrivenarchitecture/dda-pallet-commons "0.1.3-SNAPSHOT"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.1.0"]
-                 [metosin/schema-tools "0.10.0-SNAPSHOT"]
+                 [metosin/schema-tools "0.9.0"]
                  [com.palletops/pallet "0.8.12"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
