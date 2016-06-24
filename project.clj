3033314aa8ab4fa15dbf119755b0a299aa3721f8
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-06-24 08:45:14 +0200

    updated deps & integrated in masterbuild

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.1.1"]
+                 [prismatic/schema "1.1.2"]
                  [metosin/schema-tools "0.9.0"]
                  [com.palletops/pallet "0.8.12"]]
+  :plugins [[lein-sub "0.3.0"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
