8aaf33dddf0116cc33371263239bb20f31ed8b5e
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-08-04 08:10:28 +0200

    lein-release plugin: preparing 0.1.3 release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.domaindrivenarchitecture/dda-pallet-commons "0.1.3-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-pallet-commons "0.1.3"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.1.2"]
+                 [prismatic/schema "1.1.3"]
                  [metosin/schema-tools "0.9.0"]
                  [com.palletops/pallet "0.8.12"]]
   :plugins [[lein-sub "0.3.0"]]
