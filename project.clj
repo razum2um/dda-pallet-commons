45348e9c6ae07eeacf00e7d9b5cd64b8e87a7dc4
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-10-27 13:05:05 +0200

    added the option to define multiple targets per group

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject dda/dda-pallet-commons "0.4.3-SNAPSHOT"
+(defproject dda/dda-pallet-commons "0.5.0-SNAPSHOT"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]  
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [prismatic/schema "1.1.6"]
                  [metosin/schema-tools "0.9.0"]
