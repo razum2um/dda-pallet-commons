3ec1d96ef7cb2536965f3c94a100003ebeef8424
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-10-27 13:07:45 +0200

    lein-release plugin: preparing 0.5.0 release

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject dda/dda-pallet-commons "0.5.0-SNAPSHOT"
+(defproject dda/dda-pallet-commons "0.5.0"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [prismatic/schema "1.1.6"]
+                 [prismatic/schema "1.1.7"]
                  [metosin/schema-tools "0.9.0"]
                  [mvxcvi/clj-pgp "0.8.3"]
                  [com.palletops/pallet "0.8.12"]
-                 [dda/dda-config-commons "0.2.2"]
+                 [dda/dda-config-commons "0.2.3"]
                  [ch.qos.logback/logback-classic "1.2.3"]]
   :source-paths ["src" "test-utils"]
   :repositories [["snapshots" :clojars]
@@ -25,7 +25,7 @@
 
              :plugins [[lein-sub "0.3.0"]]
              :leiningen/reply
-             {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]]
+             {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
               :exclusions [commons-logging]}}
   :local-repo-classpath true
   :classifiers {:tests {:source-paths ^:replace ["test"]
