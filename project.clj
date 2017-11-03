acd85dd220f252b233a2ce932282e3f23d365406
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-11-03 16:54:59 +0100

    lein-release plugin: bumped version from 0.5.1 to 0.5.2-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet-commons "0.5.1"
+(defproject dda/dda-pallet-commons "0.5.2-SNAPSHOT"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
