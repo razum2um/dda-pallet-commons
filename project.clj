f76af65aa8a159bf270cac851978b7af7b4dbcf2
jem <michael.jerger@meissa-gmbh.de>
2018-06-13 18:28:40 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [prismatic/schema "1.1.7"]
+                 [org.clojure/tools.cli "0.3.7"]
+                 [prismatic/schema "1.1.9"]
                  [mvxcvi/clj-pgp "0.9.0"]
-                 [dda/pallet "0.9.0"]
                  [dda/dda-config-commons "1.3.1"]
                  [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                 [keypin "0.7.2"]]
+                 [dda/pallet "0.9.0"]
+                 [keypin "0.7.4"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -24,7 +24,7 @@
                                     "test/resources"]
                    :dependencies
                    [[org.clojure/test.check "0.10.0-alpha2"]
-                    [dda/pallet "0.8.13-SNAPSHOT" :classifier "tests"]
+                    [dda/pallet "0.9.0" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
                     [org.slf4j/jcl-over-slf4j "1.8.0-beta1"]]
                    :plugins
@@ -34,11 +34,11 @@
                     :exclusions [commons-logging]}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
-                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+                    :dependencies [[dda/pallet "0.9.0" :classifier "tests"]]}
              :uberjar {:source-paths ["uberjar/src"]
                        :resource-paths ["uberjar/resources"]
                        :aot :all
                        :main dda.pallet.dda-managed-ide.main
-                       :dependencies [[org.clojure/tools.cli "0.3.5"]]}}
+                       :dependencies [[org.clojure/tools.cli "0.3.7"]]}}
   :classifiers {:tests :test}
   :local-repo-classpath true)
