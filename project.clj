3afcd122dee9528f3560ee06b9c910047c83797b
jem <michael.jerger@meissa-gmbh.de>
2018-07-20 15:24:43 +0200

    Version 1.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet-commons "1.4.0"
+(defproject dda/dda-pallet-commons "1.4.1-SNAPSHOT"
   :description "common utils for dda pallet"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
